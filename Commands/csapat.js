a928d32df142f2add7e1f3f571a459c3658ebc38
Bence Tuzson <tuzsonbence2003@gmail.com>
2021-02-03 23:50:15 +0100

    csapat bug fix

--- Commands/csapat.js
+++ Commands/csapat.js
@@ -40,8 +40,11 @@ module.exports = {
                     case "rang":
                         rang = true;
                         break;
-                    default:
+                    case undefined:
                         break;
+                    default:
+                        message.channel.send("Érvénytelen paraméter!");
+                        return;
                 }
                 number = args[1];
                 break;
@@ -52,14 +55,14 @@ module.exports = {
             /*for (let i = 0; i < database.CLASS_MEMBERS.length; i++) {
                 class_members.push(database.CLASS_MEMBERS[i]);
             }*/
-            var cycle = classLength;
-            var teams = [];
+            let cycle = classLength;
+            let teams = [];
             for (let index2 = 0; index2 < number; index2++) {
                 teams.push(new Array);
                 teams[index2].push(`**${index2+1}. csapat:** `);
                 //teams[0] = index2+1 + ". csapat: ";
                 for (let index3 = 1; index3 < Math.floor(cycle/number)+1; index3++) {
-                    var randomIndex = Math.floor(Math.random()*class_members.length)
+                    let randomIndex = Math.floor(Math.random()*class_members.length)
                     const randomElement = class_members[randomIndex];
                     const index = class_members.indexOf(randomIndex);
                     /*if (index > -1) {
@@ -70,7 +73,7 @@ module.exports = {
                     teams[index2].push(args[1] === "tesi" ?  randomElement : (tag ? (message.guild.members.cache.get(randomElement.USER_ID) ? message.guild.members.cache.get(randomElement.USER_ID) : randomElement.NICKNAME) : (rang ? (message.guild.roles.cache.get(randomElement.ROLE_ID) ? message.guild.roles.cache.get(randomElement.ROLE_ID) : randomElement.NICKNAME) : randomElement.NICKNAME)));
                 }
             }
-            var used = Math.floor(cycle/number)*number;
+            let used = Math.floor(cycle/number)*number;
             for (let index5 = 0; index5 < cycle-used; index5++) {
                 teams[index5].push(args[1] === "tesi" ?  class_members[index5] : (tag ? (message.guild.members.cache.get(class_members[index5].USER_ID) ? message.guild.members.cache.get(class_members[index5].USER_ID) : class_members[index5].NICKNAME) : (rang ? (message.guild.roles.cache.get(randomElement.ROLE_ID) ? message.guild.roles.cache.get(randomElement.ROLE_ID) : class_members[index5].NICKNAME) : class_members[index5].NICKNAME)));
 
